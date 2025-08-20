92425f41adbbaf5321b4ba4cae8b146af4a43f9a
Mahak491 <shreemahak01@gmail.com>
2025-08-20 20:23:38 +0530

    feat: loader and not found

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,62 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import ProductDetails from './pages/ProductDetails';
-import Login from './pages/Login';
-import Cart from './pages/Cart';
+import { Suspense, lazy } from 'react';
 import Navbar from './components/Navbar';
-import AllProducts from './pages/AllProducts';
-import LikedProducts from './pages/LikedProducts';
-import Checkout from './pages/Checkout';
-import Signup from './pages/Signup';
-import ProtectedRoute from './pages/ProtectedRoute';
-import Category from './pages/Category';
 import ScrollToTop from './components/ScrollToTop';
+import ProtectedRoute from './pages/ProtectedRoute';
+import Loader from './components/Loader'; 
+
+
+const Home = lazy(() => import('./pages/Home'));
+const AllProducts = lazy(() => import('./pages/AllProducts'));
+const ProductDetails = lazy(() => import('./pages/ProductDetails'));
+const Login = lazy(() => import('./pages/Login'));
+const Signup = lazy(() => import('./pages/Signup'));
+const Cart = lazy(() => import('./pages/Cart'));
+const LikedProducts = lazy(() => import('./pages/LikedProducts'));
+const Checkout = lazy(() => import('./pages/Checkout'));
+const Category = lazy(() => import('./pages/Category'));
+const NotFound = lazy(() => import('./pages/NotFound')); 
 
 function App() {
   return (
     <Router>
       <Navbar />
       <ScrollToTop />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/products" element={<AllProducts />} />
-        <Route path="/product/:id" element={<ProductDetails />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/cart" element={<ProtectedRoute><Cart /></ProtectedRoute>} />
-        <Route path="/liked" element={<ProtectedRoute><LikedProducts /></ProtectedRoute>} />
-        <Route path="/checkout" element={<ProtectedRoute><Checkout /></ProtectedRoute>} />
-         <Route path="/category/:name" element={<Category />} />
-      </Routes>
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<AllProducts />} />
+          <Route path="/product/:id" element={<ProductDetails />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route
+            path="/cart"
+            element={
+              <ProtectedRoute>
+                <Cart />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/liked"
+            element={
+              <ProtectedRoute>
+                <LikedProducts />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/checkout"
+            element={
+              <ProtectedRoute>
+                <Checkout />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="/category/:name" element={<Category />} />
+          <Route path="*" element={<NotFound />} /> {/* 404 fallback */}
+        </Routes>
+      </Suspense>
     </Router>
   );
 }
---
+++ src/components/Loader.jsx
@@ -0,0 +1,9 @@
+const Loader = () => {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <p>Loading...</p>
+    </div>
+  );
+};
+
+export default Loader;
